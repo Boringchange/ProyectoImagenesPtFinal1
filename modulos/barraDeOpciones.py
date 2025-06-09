5e76380dd3910175875fc9bf1737f27d15820683
Omar Jesús Vázquez Sánchez <omar_jv.s@hotmail.com>
2025-06-09 11:55:04 -0600

    Presentable

--- modulos/barraDeOpciones.py
+++ modulos/barraDeOpciones.py
@@ -1,10 +1,16 @@
+import os.path
+
+import cv2
 from PyQt6.QtCore import QPoint, Qt
-from PyQt6.QtGui import QIcon, QAction, QPalette, QFont
+from PyQt6.QtGui import QIcon, QAction, QPalette, QFont, QImage, QPixmap
 from PyQt6.QtWidgets import QLabel, QToolButton, QListWidget, QPushButton, QFrame, QVBoxLayout, QScrollArea, QScrollBar, \
-    QWidget, QHBoxLayout
+    QWidget, QHBoxLayout, QDialog, QLineEdit, QComboBox
 import json
 
-from scripts.ProcesamientoImagenes import mostrar_imagenes_rgb, conversion_grises, mostrar_histograma
+from scripts.ProcesamientoImagenes import mostrar_imagenes_rgb, conversion_grises, mostrar_histograma, \
+    operaciones_aritmeticas_imagen, deteccion_objetos, operacion_not, operaciones_entre_2_imagenes, \
+    operaciones_umbralizacion, operacion_umbral_otsu, operacion_ecualizacion_exponencial, operaciones_ruido, \
+    operacion_filtro_mediana, operacion_operador_prewitt, operacion_filtro_segmentacion_completa_multiumbralizacion
 
 
 class DesplegableBotones(QFrame):
@@ -132,6 +138,58 @@ class BarraHerramientas:
             self.ventana.barra_ventanas.mostrar_imagen(self.ventana.objeto_actual)
         elif nombre == "Histograma":
             mostrar_histograma(self.ventana.objeto_actual)
+        elif nombre == "Detección de objetos":
+            deteccion_objetos(self.ventana.objeto_actual)
+        elif nombre == "Suma":
+            self.llamar_funciones(operaciones_aritmeticas_imagen, [self.ventana.objeto_actual, 1])
+        elif nombre == "Resta":
+            self.llamar_funciones(operaciones_aritmeticas_imagen, [self.ventana.objeto_actual, 2])
+        elif nombre == "Multiplicación":
+            self.llamar_funciones(operaciones_aritmeticas_imagen, [self.ventana.objeto_actual, 3])
+        elif nombre == "NOT":
+            imagen = operacion_not(self.ventana.objeto_actual)
+            self.ventana.objeto_actual.realizar_cambio(imagen)
+            self.ventana.barra_ventanas.mostrar_imagen(self.ventana.objeto_actual)
+        elif nombre == "Suma 2 imagenes":
+            self.llamar_funciones_entre_2_imagenes(operaciones_entre_2_imagenes, 1)
+        elif nombre == "Resta 2 imagenes":
+            self.llamar_funciones_entre_2_imagenes(operaciones_entre_2_imagenes, 2)
+        elif nombre == "Multiplicación 2 imagenes":
+            self.llamar_funciones_entre_2_imagenes(operaciones_entre_2_imagenes, 3)
+        elif nombre == "OR":
+            self.llamar_funciones_entre_2_imagenes(operaciones_entre_2_imagenes, 4)
+        elif nombre == "AND":
+            self.llamar_funciones_entre_2_imagenes(operaciones_entre_2_imagenes, 5)
+        elif nombre == "XOR":
+            self.llamar_funciones_entre_2_imagenes(operaciones_entre_2_imagenes, 6)
+        elif nombre == "Umbral simple":
+            self.llamar_funciones(operaciones_umbralizacion, [self.ventana.objeto_actual, 0])
+        elif nombre == "Umbral adaptativo":
+            self.llamar_funciones(operaciones_umbralizacion, [self.ventana.objeto_actual, 3])
+        elif nombre == "Umbral Otsu":
+            imagen = operacion_umbral_otsu(self.ventana.objeto_actual)
+            self.ventana.objeto_actual.realizar_cambio(imagen)
+            self.ventana.barra_ventanas.mostrar_imagen(self.ventana.objeto_actual)
+        elif nombre == "Umbral binario inverso":
+            self.llamar_funciones(operaciones_umbralizacion, [self.ventana.objeto_actual, 1])
+        elif nombre == "Exponencial":
+            self.llamar_funciones(operacion_ecualizacion_exponencial, [self.ventana.objeto_actual])
+        elif nombre == "Mediana":
+            self.llamar_funciones(operacion_filtro_mediana, [self.ventana.objeto_actual])
+        elif nombre == "Operador de Prewitt":
+            imagen = operacion_operador_prewitt(self.ventana.objeto_actual)
+            self.ventana.objeto_actual.realizar_cambio(imagen)
+            self.ventana.barra_ventanas.mostrar_imagen(self.ventana.objeto_actual)
+        elif nombre == "Sal y Pimienta":
+            imagen = operaciones_ruido(self.ventana.objeto_actual, 1)
+            self.ventana.objeto_actual.realizar_cambio(imagen)
+            self.ventana.barra_ventanas.mostrar_imagen(self.ventana.objeto_actual)
+        elif nombre == "Gaussiano":
+            imagen = operaciones_ruido(self.ventana.objeto_actual, 2)
+            self.ventana.objeto_actual.realizar_cambio(imagen)
+            self.ventana.barra_ventanas.mostrar_imagen(self.ventana.objeto_actual)
+        elif nombre == "Segmentación completa mediante Multiumbralización":
+            self.llamar_funciones(operacion_filtro_segmentacion_completa_multiumbralizacion, [self.ventana.objeto_actual])
 
     def deshacer_cambio(self):
         self.ventana.objeto_actual.imagen_actual = self.ventana.objeto_actual.imagen_actual - 1
@@ -144,3 +202,111 @@ class BarraHerramientas:
     def reiniciar_imagen(self):
         self.ventana.objeto_actual.reiniciar_imagen()
         self.ventana.barra_ventanas.mostrar_imagen(self.ventana.objeto_actual)
+
+    def llamar_funciones(self, funcion, parametros):
+
+        self.ventana_flotante = QWidget()
+        self.ventana_flotante.closeEvent = self.cerrando_widget_informacion
+        self.ventana_flotante.setWindowTitle("Ingresar valores")
+        self.ventana_flotante.resize(400, 70)
+        ventana_flotante_layout = QHBoxLayout()
+        entrada = QLineEdit()
+        boton_confirmar_cambio = QPushButton("Confirmar cambio")
+
+        ventana_flotante_layout.addWidget(QLabel("Ingrese el valor para modificar"))
+        ventana_flotante_layout.addWidget(entrada)
+        ventana_flotante_layout.addWidget(boton_confirmar_cambio)
+
+        entrada.textChanged.connect(lambda: self.cambiar_cuando_cambia(funcion, parametros, entrada.text()))
+        boton_confirmar_cambio.clicked.connect(lambda: self.guardar_cambio_cerrar_ventana())
+
+        self.ventana_flotante.setLayout(ventana_flotante_layout)
+
+        self.ventana_flotante.show()
+
+    def cambiar_cuando_cambia(self, funcion, parametros, numero):
+
+        try:
+            if funcion == operaciones_entre_2_imagenes:
+                imagen1 = parametros[0]
+                imagen2 = self.seleccionador.itemData(parametros[1])
+
+                parametros[0] = imagen1
+                parametros[1] = imagen2
+
+                print(parametros[0])
+                print(parametros[1])
+                print(numero)
+
+            self.imagen_cambiada = funcion(*parametros, numero)
+        except Exception as e:
+            print(e)
+            return
+
+        if self.imagen_cambiada is None:
+            return
+
+        imagen_qimage = None
+
+        if len(self.imagen_cambiada.shape) == 2:
+            # Imagen en escala de grises
+            h, w = self.imagen_cambiada.shape
+            bytes_por_linea = w
+            imagen_qimage = QImage(self.imagen_cambiada.data, w, h, bytes_por_linea, QImage.Format.Format_Grayscale8)
+        else:
+            # Imagen color BGR a RGB
+            h, w, ch = self.imagen_cambiada.shape
+            bytes_por_linea = ch * w
+            imagen_rgb = cv2.cvtColor(self.imagen_cambiada, cv2.COLOR_BGR2RGB)
+            imagen_qimage = QImage(imagen_rgb.data, w, h, bytes_por_linea, QImage.Format.Format_RGB888)
+
+        imagen_pixmap = QPixmap.fromImage(imagen_qimage)
+
+        self.ventana.label_imagen.setPixmap(imagen_pixmap)
+        self.ventana.label_imagen.resize(imagen_pixmap.width(), imagen_pixmap.height())
+
+    def guardar_cambio_cerrar_ventana(self):
+        if self.imagen_cambiada is not None:
+            self.ventana.objeto_actual.realizar_cambio(self.imagen_cambiada)
+            self.ventana.barra_ventanas.mostrar_imagen(self.ventana.objeto_actual)
+            self.ventana_flotante.close()
+        else:
+            return
+
+
+    def llamar_funciones_entre_2_imagenes(self, funcion, numero):
+
+        self.ventana_flotante = QWidget()
+        self.ventana_flotante.closeEvent = self.cerrando_widget_informacion
+        self.ventana_flotante.setWindowTitle("Seleccione la segunda imagen")
+        self.ventana_flotante.resize(400, 70)
+        ventana_flotante_layout = QHBoxLayout()
+        self.seleccionador = QComboBox()
+        self.seleccionador.addItem("Escoja una opcion", None)
+
+        for objeto in self.ventana.imagenes_agregadas:
+
+            if objeto == self.ventana.objeto_actual:
+                continue
+            ruta_imagen = objeto.imagen_linea_tiempo[objeto.imagen_actual]
+            nombre = os.path.splitext(os.path.basename(ruta_imagen))[0]
+
+            self.seleccionador.addItem(nombre)
+            self.seleccionador.setItemData(self.seleccionador.count() - 1, objeto)
+
+        boton_confirmar_cambio = QPushButton("Confirmar cambio")
+
+        ventana_flotante_layout.addWidget(QLabel("Seleccione la segunda imagen"))
+        ventana_flotante_layout.addWidget(self.seleccionador)
+        ventana_flotante_layout.addWidget(boton_confirmar_cambio)
+
+        self.seleccionador.currentIndexChanged.connect(lambda index: self.cambiar_cuando_cambia(funcion, [self.ventana.objeto_actual, index], numero))
+        boton_confirmar_cambio.clicked.connect(lambda: self.guardar_cambio_cerrar_ventana())
+
+        self.ventana_flotante.setLayout(ventana_flotante_layout)
+
+        self.ventana_flotante.show()
+
+    def cerrando_widget_informacion(self,event):
+        self.ventana.barra_ventanas.mostrar_imagen(self.ventana.objeto_actual)
+        event.accept()
--- scripts/ProcesamientoImagenes.py
+++ scripts/ProcesamientoImagenes.py
@@ -17,7 +17,7 @@ def seleccionar_imagen(imagen):
 def obtener_imagen_objeto(objeto:Imagen):
     ruta_imagen = objeto.imagen_linea_tiempo[objeto.imagen_actual]
 
-    return cv2.imread(ruta_imagen, cv2.IMREAD_UNCHANGED)
+    return cv2.cvtColor(cv2.imread(ruta_imagen, cv2.IMREAD_UNCHANGED), cv2.COLOR_BGR2RGB)
 
 def mostrar_imagenes_rgb(objeto: Imagen):
 
@@ -88,3 +88,263 @@ def mostrar_histograma(objeto:Imagen):
     cv2.imshow("Histograma colores", histogramaColores)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+def operaciones_aritmeticas_imagen(objeto, operacion, numero):
+
+    imagen = obtener_imagen_objeto(objeto)
+
+    imagen_resultado = None
+
+    if operacion == 1:
+        imagen_resultado = cv2.add(imagen, int(numero))
+    elif operacion == 2:
+        imagen_resultado = cv2.subtract(imagen, int(numero))
+    elif operacion == 3:
+        imagen_resultado = cv2.multiply(imagen, float(numero))
+
+    return imagen_resultado
+
+def operacion_not(objeto:Imagen):
+    imagen = obtener_imagen_objeto(objeto)
+    imagen_resultado = cv2.bitwise_not(imagen)
+
+    return cv2.cvtColor(imagen_resultado, cv2.COLOR_BGR2RGB)
+
+def deteccion_objetos(objeto:Imagen):
+    imagen1 = obtener_imagen_objeto(objeto)
+
+    hsv = cv2.cvtColor(imagen1, cv2.COLOR_BGR2HSV)
+    lower_white = np.array([0, 0, 200])
+    upper_white = np.array([180, 30, 255])
+
+    mascara = cv2.inRange(hsv, lower_white, upper_white)
+
+    contornos, _ = cv2.findContours(mascara, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    for c in contornos:
+        x, y, w, h = cv2.boundingRect(c)
+        cv2.rectangle(imagen1, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+    cv2.imshow("Detección de objetos por bordes", imagen1)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def operaciones_entre_2_imagenes(objeto1, objeto2, operacion):
+    imagen1 = obtener_imagen_objeto(objeto1)
+
+    imagen2 = obtener_imagen_objeto(objeto2)
+
+    imagen2 = cv2.resize(imagen2, (imagen1.shape[:2][1], imagen1.shape[:2][0]))
+
+    operacion_imagen = None
+
+    if operacion == 1:
+        operacion_imagen = cv2.add(imagen1, imagen2)
+    elif operacion == 2:
+        operacion_imagen = cv2.subtract(imagen1, imagen2)
+    elif operacion == 3:
+        operacion_imagen = cv2.multiply(imagen1, imagen2)
+    elif operacion == 4:
+        operacion_imagen = cv2.bitwise_or(imagen1, imagen2)
+    elif operacion == 5:
+        operacion_imagen = cv2.bitwise_and(imagen1, imagen2)
+    elif operacion == 6:
+        operacion_imagen = cv2.bitwise_xor(imagen1, imagen2)
+
+    return cv2.cvtColor(operacion_imagen, cv2.COLOR_BGR2RGB)
+
+def operaciones_umbralizacion(objeto:Imagen, operacion, numero):
+    numero = int(numero)
+
+    imagen = obtener_imagen_objeto(objeto)
+
+    if len(imagen.shape) == 3:
+        imagen = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
+
+    resultado = None
+
+    if operacion == 0:  # Umbral binario simple
+        _, resultado = cv2.threshold(imagen, numero, 255, cv2.THRESH_BINARY)
+
+    elif operacion == 1:  # Umbral binario inverso
+        _, resultado = cv2.threshold(imagen, numero, 255, cv2.THRESH_BINARY_INV)
+
+    elif operacion == 3:  # Umbral adaptativo (media)
+        resultado = cv2.adaptiveThreshold(imagen, 255,
+                                          cv2.ADAPTIVE_THRESH_MEAN_C,
+                                          cv2.THRESH_BINARY, 11, numero)
+
+    elif operacion == 4:  # Umbral adaptativo (gaussiano)
+        resultado = cv2.adaptiveThreshold(imagen, 255,
+                                          cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                          cv2.THRESH_BINARY, 11, numero)
+
+    else:
+        raise ValueError("Operación no válida (debe ser 0 a 4)")
+
+    # Guardar la imagen resultante en el objeto
+    return resultado
+
+def operacion_umbral_otsu(objeto:Imagen):
+    imagen = obtener_imagen_objeto(objeto)
+
+    if len(imagen.shape) == 3:
+        imagen = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
+
+    _, resultado = cv2.threshold(imagen, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+    return resultado
+
+def obtener_valores_ecualizacion(intensidad):
+    if intensidad is None:
+        return
+    hist = cv2.calcHist([intensidad], [0], None, [256], [0, 256]).flatten()
+
+    probabilidad = hist / hist.sum()
+    probabilidad_acumulada = np.cumsum(probabilidad)
+    valor_minimo = np.min(intensidad)
+
+    return valor_minimo, probabilidad_acumulada
+
+def operacion_ecualizacion_exponencial(objeto:Imagen, numero):
+
+    imagen = obtener_imagen_objeto(objeto)
+
+    numero = float(numero)
+
+    if numero <= 0:
+        return None
+
+    imagen_ycrcb = cv2.cvtColor(imagen, cv2.COLOR_RGB2YCrCb)
+
+    y, cr, cb = cv2.split(imagen_ycrcb)
+
+    valor_minimo, probabilidad_acumulada = obtener_valores_ecualizacion(y)
+
+    y_nueva = np.zeros_like(y)
+
+    filas, columnas = y.shape
+
+    for i in range(filas):
+        for j in range(columnas):
+            epsilon = 1e-8
+            valor = 1 - probabilidad_acumulada[y[i][j]]
+            valor = max(valor, epsilon)
+            y_nueva[i, j] = valor_minimo - (1 / numero) * np.log(valor)
+
+    y_nueva = np.clip(y_nueva, 0, 255)
+    y_nueva = y_nueva.astype(np.uint8)
+
+    imagen_modificada = cv2.merge((y_nueva, cr, cb))
+    imagen_resultado = cv2.cvtColor(imagen_modificada, cv2.COLOR_YCrCb2RGB)
+
+    return imagen_resultado
+
+def operacion_filtro_mediana(objeto:Imagen, numero):
+    imagen = obtener_imagen_objeto(objeto)
+
+    print("Hola")
+    print(numero)
+
+    numero = int(numero)
+
+    if (numero % 2) == 0:
+        return None
+
+    # Función para hacer el filtro mediana
+    imagen_resultado = cv2.medianBlur(imagen, int(numero))
+
+    return cv2.cvtColor(imagen_resultado, cv2.COLOR_BGR2RGB)
+
+def operacion_operador_prewitt(objeto:Imagen):
+    imagen = obtener_imagen_objeto(objeto)
+
+    kernel_prewitt_x = np.array([[1, 0, -1], [1, 0, -1], [1, 0, -1]], dtype=np.float32)
+    kernel_prewitt_y = np.array([[1, 1, 1], [0, 0, 0], [-1, -1, -1]], dtype=np.float32)
+
+    bordes_prewitt_x = cv2.filter2D(imagen, -1, kernel_prewitt_x)
+    bordes_prewitt_y = cv2.filter2D(imagen, -1, kernel_prewitt_y)
+
+    imagen_resultado = cv2.addWeighted(bordes_prewitt_x, 0.5, bordes_prewitt_y, 0.5, 0)
+
+    return imagen_resultado
+
+def operaciones_ruido(objeto: Imagen, opcion: int):
+    imagen = obtener_imagen_objeto(objeto)
+    imagen_resultado = imagen.copy()
+
+    if opcion == 1:
+        # --- Ruido sal y pimienta ---
+        prob = 0.02  # proporción de píxeles con ruido
+        output = imagen_resultado.copy()
+        if len(imagen.shape) == 2:
+            # Imagen en gris
+            black = 0
+            white = 255
+        else:
+            # Imagen en color
+            black = [0, 0, 0]
+            white = [255, 255, 255]
+
+        total_pixels = imagen_resultado.shape[0] * imagen_resultado.shape[1]
+        num_salt = int(total_pixels * prob / 2)
+        num_pepper = int(total_pixels * prob / 2)
+
+        # Sal (blanco)
+        for _ in range(num_salt):
+            i = np.random.randint(0, imagen_resultado.shape[0])
+            j = np.random.randint(0, imagen_resultado.shape[1])
+            output[i, j] = white
+
+        # Pimienta (negro)
+        for _ in range(num_pepper):
+            i = np.random.randint(0, imagen_resultado.shape[0])
+            j = np.random.randint(0, imagen_resultado.shape[1])
+            output[i, j] = black
+
+        imagen_resultado = output
+
+    elif opcion == 2:
+        # --- Ruido gaussiano ---
+        mean = 0
+        std = 25  # desviación estándar
+        gauss = np.random.normal(mean, std, imagen.shape).astype(np.float32)
+        noisy = imagen.astype(np.float32) + gauss
+        imagen_resultado = np.clip(noisy, 0, 255).astype(np.uint8)
+
+    return cv2.cvtColor(imagen_resultado, cv2.COLOR_BGR2RGB)
+
+def operacion_filtro_segmentacion_completa_multiumbralizacion(objeto:Imagen, texto):
+    if texto is None or texto == "":
+        return None
+
+    try:
+        variables = [int(x) for x in texto.split(',')]
+    except ValueError:
+        return
+
+    variables.sort()
+
+    imagen = obtener_imagen_objeto(objeto)
+
+    if len(imagen.shape) == 3:
+        imagen = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
+
+    imagenCopiaGris = imagen.copy()
+    separacionGrises = 255/(len(variables)+1)
+    segmentacion = np.zeros_like(imagenCopiaGris)
+
+
+    for i in range(len(variables)):
+        if i == 0:
+            segmentacion[imagenCopiaGris <= variables[i]] = separacionGrises * (i + 1)
+            continue
+        if i == len(variables):
+            segmentacion[imagenCopiaGris > variables[i - 1]] = separacionGrises * (i + 1)
+            continue
+        segmentacion[(imagenCopiaGris > variables[i - 1]) & (imagenCopiaGris <= variables[i])] = separacionGrises * (i + 1)
+
+    imagen_resultado = segmentacion.copy()
+
+    return imagen_resultado
